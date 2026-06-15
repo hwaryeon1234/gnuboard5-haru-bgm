823bcc589ea7bd4533a3a2d65bf24c775a11d945
화련 <101951536+hwaryeon1234@users.noreply.github.com>
2026-06-16 01:50:41 +0900

    Delete plugin/haru_bgm directory

--- plugin/haru_bgm/admin/_common.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once(dirname(__FILE__).'/../_common.php');
-if (!$is_admin) {
-    if (defined('HB_JSON_MODE') && HB_JSON_MODE) {
-        hb_json_exit(array('ok' => false, 'message' => 'admin_required'));
-    }
-    alert('관리자만 접근할 수 있습니다.', G5_URL);
-}
-?>
