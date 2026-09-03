d3f3b8911aa3733a404df3290e2945daeea6ef5f
화련 <101951536+hwaryeon1234@users.noreply.github.com>
2026-09-04 02:25:36 +0900

    Delete 하루bgm directory

--- 하루bgm/extend/haru_bgm.extend.php
+++
@@ -1,6 +0,0 @@
-<?php
-// 하루BGM 사이트 전체 방송 bootstrap.
-if (!defined('_GNUBOARD_')) return;
-$hb_sitewide_file = (defined('G5_PLUGIN_PATH') ? G5_PLUGIN_PATH : dirname(__DIR__).'/plugin').'/haru_bgm/sitewide.php';
-if (is_file($hb_sitewide_file)) include_once($hb_sitewide_file);
-unset($hb_sitewide_file);
